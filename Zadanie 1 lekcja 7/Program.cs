42b3894e9665548abecffffa488932acf33770a5
HARDPC <HARDPC@WOTUM>
2023-12-20 17:30:54 +0100

    Add project files.

---
+++ Zadanie 1 lekcja 7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KatoApp
+{
+
+   class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Nadanie zmiennym int wartości
+            int a = 5; 
+            int b = 5;
+
+            //Porównanie zmiennych przy użyciu operatora 
+            if (a == b)
+            {
+                Console.WriteLine(a + " i " + b + " są równe ");
+            }
+            else
+            {
+                Console.WriteLine(a + " i " + b + " nie są równe ");
+            }
+        }
+
+    }
+
+
+}
